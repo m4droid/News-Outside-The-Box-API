ab966b661bcd9c85748418822b91fba84c00afc8
Francisco Madrid <fjmadrid@gmail.com>
2017-04-09 00:24:26 -0300

    read data from request

--- notb_api/wsgi.py
+++ notb_api/wsgi.py
@@ -1,4 +1,6 @@
 import json
+import re
+from urllib.parse import urlparse, unquote
 
 from elasticsearch import Elasticsearch
 
@@ -12,12 +14,39 @@ app = Flask(__name__)
 INDEX = 'totb'
 DOCUMENTS = 'news'
 
-THE_OTHER_SIDE = {
-    'www.emol.com': [
-        'www.asdf.com',
-        'www.elmostrador.cl',
-    ]
-}
+LEFT_EVIL = [
+    'lanacion.cl',
+    'adnradio.cl',
+    '24horas.cl',
+    'theclinic.cl',
+    'biobiochile.cl',
+    'noticias.terra.cl',
+]
+
+RIGHT_EVIL = [
+    'emol.com',
+    'latercera.com',
+    'impresa.lasegunda.com',
+    't13.cl',
+    'mega.cl',
+    'lahora.cl',
+    'publimetro.cl',
+    'eldinamo.cl',
+]
+
+NEUTRAL_EVIL = [
+    'cooperativa.cl',
+    'terra.cl',
+    'cnnchile.com',
+    'publimetro.cl',
+    'df.cl',
+    'pulso.cl',
+    'ciperchile.cl',
+]
+
+THE_OTHER_SIDE = {s: list(RIGHT_EVIL) for s in LEFT_EVIL}
+THE_OTHER_SIDE.update({s: list(LEFT_EVIL) for s in RIGHT_EVIL})
+THE_OTHER_SIDE.update({s: list(LEFT_EVIL) + list(RIGHT_EVIL) for s in NEUTRAL_EVIL})
 
 
 def analyzer_mapper(analyzer):
@@ -48,10 +77,24 @@ es.indices.put_mapping(
 
 @app.route('/news', methods=['GET', 'POST'])
 def news_search():
-    data = request.json or {
-        'header': request.args.get('header') or '',
-        'subheader': request.args.get('subheader') or ''
-    }
+    if request.method == 'POST':
+        data = request.json
+    else:
+        data = {
+            'url': request.args.get('header') or '',
+            'header': request.args.get('header') or '',
+            'subheader': request.args.get('subheader') or ''
+        }
+
+    news_headers = [data.get('header') or '', data.get('subheader') or ''] if data is not None else []
+
+    if data.get('url') is not None:
+        matcher = re.match(r'https:\/\/l\.facebook.com\/l\.php\?u=(.*)&.*$', data['url'])
+        if matcher is not None:
+            uri = urlparse(unquote(matcher.group(1)))
+            data['source'] = uri.netloc.replace('www.', '')
+    if 'source' not in data:
+        data['source'] = None
 
     es_query = {
         "query": {
@@ -60,7 +103,7 @@ def news_search():
                     {
                         "more_like_this": {
                             "fields": ["header", "subheader"],
-                            "like": ' '.join([data['header'], data['subheader']]).strip(),
+                            "like": ' '.join(news_headers).strip(),
                             "min_term_freq": 1,
                             "min_doc_freq": 1
                         }
@@ -76,7 +119,7 @@ def news_search():
     }
 
     es_query['query']['bool']['filter']['bool']['should'] = [
-        {"term": {"url": source}} for source in THE_OTHER_SIDE.get('www.emol.com') or []
+        {"term": {"url": source}} for source in (THE_OTHER_SIDE.get(data['source']) or [])
     ]
 
     es_response = es.search(index=INDEX, doc_type=DOCUMENTS, body=es_query)
